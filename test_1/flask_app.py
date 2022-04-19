70a222880cd649ca872e7c1356d0e604fe53dd68
Bryan <78073695+Trelaquix@users.noreply.github.com>
2022-04-19 15:58:58 +0800

    Update flask_app.py

--- test_1/flask_app.py
+++ test_1/flask_app.py
@@ -5,31 +5,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return ('flask webservice - activity 3')
+   return ('flask webservice - activity 2')
 
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
    # get the incoming JSON structure
    data = request.get_json(silent=True)
+   # data is a dictionary
+   # data = request.get_json(silent=True)
+  
+   # get the dictionary value for queryResult
+   # queryResult is a dictionary
+   # queryResult = data['queryResult']
+  
+   # from the queryResult dictionary
+   # get the value for the key action
+   # somevalue = dictionary['key']
+   # action is our variable name
+   # action in queryResult['action'] is the key name
+   # action = queryResult['action']
+   
    # get the action name
    action = ""
    if 'action' in data['queryResult']:
        action = data['queryResult']['action']
    # conditional block to call relevant method for action
-   if (action == 'test_connection'):
+   # action name must be the same as dialogflow > intents > action
+   if (action == 'test_connection'): 
        return test_connection()
-   elif (action == 'get_projectnames'):
-       return get_projectnames(data)
    else:
        return no_implementation(data, action)
-
-
-def get_projectnames(data):
-   reply = {}
-   reply["fulfillmentText"] = "The list of active projects are:"
-   reply["fulfillmentMessages"] = []
-   return jsonify(reply)
 
 
 def test_connection():
@@ -41,7 +47,7 @@ def test_connection():
 
 def no_implementation(data, action):
    intent_name = data['queryResult']['intent']['displayName']
-   fulfillment_text = " "
+   fulfillment_text = ""
    if action == "":
        fulfillment_text = "No action name specified for intent " + intent_name
    else:
